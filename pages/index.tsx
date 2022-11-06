f9c20a1ed92a799143c09feaa74c2433844e0e3f
Jesper Hodge <jesper.hodge@rtl.de>
2022-11-06 14:42:16 +0100

    add html stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,12 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center">
+      <div className="text-2xl text-center">Which Pokémon is rounder?</div>
+      <div className="border rounded p-8 flex justify-between">
+        <div></div>
+      </div>
+    </div>
+  );
 }
